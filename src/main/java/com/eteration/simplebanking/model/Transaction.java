c47b334787b5052be7acf616aa6b269bd09796a1
burcak.ozgul <burcak.ozgul@hititcs.com>
2022-03-06 18:24:08 +0300

    project created

--- src/main/java/com/eteration/simplebanking/model/Transaction.java
+++ src/main/java/com/eteration/simplebanking/model/Transaction.java
@@ -6,7 +6,6 @@ import javax.persistence.Column;
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,7 +14,6 @@ import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
@@ -23,7 +21,7 @@ import lombok.Data;
 @Entity
 @Table(name = "transaction")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "transaction_type" , discriminatorType = DiscriminatorType.STRING)
+@DiscriminatorColumn(name = "transaction_type", discriminatorType = DiscriminatorType.STRING)
 public abstract class Transaction {
     @JsonIgnore
     @Id
